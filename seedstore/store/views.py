6f7f4363e74982560a5860cd1f6c48727e14d1ff
sayooj <msayooj95@gmail.com>
2023-09-21 23:04:12 +0530

    signup and login without validation

--- seedstore/store/views.py
+++ seedstore/store/views.py
@@ -4,3 +4,10 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'index.html')
+
+
+def signup(request):
+    return render(request, 'signup.html')
+
+def login(request):
+    return render(request, 'login.html')
